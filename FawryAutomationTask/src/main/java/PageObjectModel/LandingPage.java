93a8c2e1d3c777b4684a3d3969b7565df340bc45
omarsheds <omarsheds@gmail.com>
2023-05-28 20:51:06 +0300

    update

--- FawryAutomationTask/src/main/java/PageObjectModel/LandingPage.java
+++ FawryAutomationTask/src/main/java/PageObjectModel/LandingPage.java
@@ -8,12 +8,24 @@ public class LandingPage {
     WebDriver driver;
     By allDropdown = By.id("nav-hamburger-menu");
     By loginButton = By.id("hmenu-customer-name");
+    By seeAllButton = By.cssSelector("[class=\"hmenu-item hmenu-compressed-btn\"]");
 
+    By gamesOption = By.cssSelector("a[data-menu-id=\"16\"]");
+    By allVideoGamesOption = By.xpath("//a[text()=\"All Video Games\"]");
 
-    public LandingPage(WebDriver driver){this.driver=driver}
+    public LandingPage(WebDriver driver){this.driver=driver;}
 
-    public LandingPage clickDropdown(){
-        driver.findElement(dropdownLink).click();
-        return this;
+    public LoginPage clickOnLoginButton(){
+        driver.findElement(allDropdown).click();
+        driver.findElement(loginButton).click();
+        return new LoginPage(driver);
+    }
+
+    public VideoGamesPage chooseAllVideoGamesFromAllDropdown(){
+        driver.findElement(allDropdown).click();
+        driver.findElement(seeAllButton).click();
+        driver.findElement(gamesOption);
+        driver.findElement(allVideoGamesOption);
+        return new VideoGamesPage(driver);
     }
 }
